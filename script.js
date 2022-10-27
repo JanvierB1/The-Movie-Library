76b5764c7f03a87b0cd1f4a1cdbba5a9803cc1cc
Z. R <zmirza7701@gmail.com>
2022-10-27 18:56:41 -0500

    added code to show NYT movie info

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var moviePoster = document.getElementById('movie-poster')
 var imdbRating = document.getElementById('imdb-ratings')
 var metaRating = document.getElementById('meta-ratings')
 var rottenRating = document.getElementById('rotten-ratings')
+var movieRating = document.getElementById('movie-rating')
 var movieName = 'Deadpool'
 
 var apiUrl = `https://imdb-api.com/API/SearchMovie/k_jisxuuy3/${movieName}`
@@ -43,3 +44,35 @@ function init() {
 
 console.log(apiUrl);
 apiCall(apiUrl);
+
+
+var requestUrl = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?&api-key=R0eFfTq42Mk2pofP6Ums8HKtAedFJequ`
+
+function apiCall(requestUrl) {
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=deadpool&api-key=R0eFfTq42Mk2pofP6Ums8HKtAedFJequ`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (movieRating) {
+                    console.log(movieRating.results[0].display_title)
+                    var movieName = movieRating.results
+                    var title 
+                    var rating 
+                    for (var i = 0; i < movieName.length; i++) {
+                        if (movieName[i].display_title === "Deadpool"){
+                            title = movieName[i].display_title
+                            rating = movieName[i].mpaa_rating
+                        } 
+                    }
+                    console.log(title);
+                    console.log(rating)
+                })
+        })
+    }
+
